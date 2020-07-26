1fe9f4e89e8654a4b53483145496f5dada5327f2
TncBogdan <tncbogdan@gmail.com>
2020-07-26 10:50:02 +0300

    Inplement map based service. Close#6

---
+++ pet-clinic-data/src/main/java/com/tnc/services/map/PetServiceMap.java
@@ -0,0 +1,33 @@
+package com.tnc.services.map;
+
+import com.tnc.model.Pet;
+import com.tnc.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}

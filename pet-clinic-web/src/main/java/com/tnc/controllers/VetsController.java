9c46d4373d49e9f49da7d64890923a58948a653f
TncBogdan <tncbogdan@gmail.com>
2020-07-29 18:16:51 +0300

    List all vets. Closes #15

--- pet-clinic-web/src/main/java/com/tnc/controllers/VetsController.java
+++ pet-clinic-web/src/main/java/com/tnc/controllers/VetsController.java
@@ -1,12 +1,23 @@
 package com.tnc.controllers;
 
+import com.tnc.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/vets")
 @Controller
 public class VetsController {
-    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-    public String listVets() {
+
+    VetService vetService;
+
+    public VetsController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }

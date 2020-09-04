381a3866b68a7ffe41fe053ddc9539beb2ed0834
TncBogdan <tncbogdan@gmail.com>
2020-09-04 22:00:06 +0300

    Added spring data jpa. Closes #40

--- pet-clinic-data/src/main/java/com/tnc/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/tnc/repositories/OwnerRepository.java
@@ -1,6 +1,10 @@
 package com.tnc.repositories;
 
+import com.tnc.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
-public interface OwnerRepository extends CrudRepository<OwnerRepository, Long> {
+public interface OwnerRepository extends CrudRepository<Owner, Long> {
+
+    Owner findByLastName(String lastName);
+
 }

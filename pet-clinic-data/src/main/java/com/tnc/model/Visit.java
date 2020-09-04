35511950b11af9db88aa5d6de3f619efd3a2ba42
TncBogdan <tncbogdan@gmail.com>
2020-09-04 21:08:34 +0300

    Added visit entity. Closes #38

--- pet-clinic-data/src/main/java/com/tnc/model/Visit.java
+++ pet-clinic-data/src/main/java/com/tnc/model/Visit.java
@@ -1,11 +1,18 @@
 package com.tnc.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity {
 
+    @Column(name = "date")
     private LocalDate date;
+    @Column(name = "description")
     private String description;
+    @ManyToMany
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 
     public LocalDate getDate() {

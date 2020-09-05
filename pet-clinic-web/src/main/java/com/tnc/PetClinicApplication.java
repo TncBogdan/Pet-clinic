07bcfd2a1643df4033f32b08f9c667f2d125d854
TncBogdan <tncbogdan@gmail.com>
2020-09-05 07:59:47 +0300

    Added spring data jpa pet service. Closes #42

--- pet-clinic-web/src/main/java/com/tnc/PetClinicApplication.java
+++ pet-clinic-web/src/main/java/com/tnc/PetClinicApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//@ComponentScan(com.tnc.services.map)
+//@ComponentScan(com.tnc.services.springdatajpa.OwnerSDJpaService)
 public class PetClinicApplication {
 
 	public static void main(String[] args) {
